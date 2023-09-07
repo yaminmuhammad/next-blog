62ff234968dfc4111bbfb6f69e630679b8be8cde
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-09-07 09:03:12 +0700

    initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,19 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+	title: "Blog App",
+	description: "The best blog app!",
+};
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<div className="container">
+				<div className="wrapper">{children}</div>
+			</div>
+		</html>
+	);
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import styles from "./homepage.module.css";
+
+export default function Home() {
+	return (
+		<div>
+			<Link href="/">
+				<h1>
+					Consequat quis minim aliquip Lorem veniam culpa ipsum Lorem fugiat ex excepteur adipisicing velit
+					fugiat.
+				</h1>
+				Laboris do mollit velit nostrud consequat ipsum.Dolor excepteur officia id eu esse occaecat in qui
+				aliqua in fugiat. Incididunt aute magna non laborum excepteur occaecat qui sit in excepteur Lorem
+				culpa qui occaecat. Reprehenderit esse sit excepteur tempor. Ut est sunt ea do non veniam mollit
+				cillum amet nisi est. Id dolor exercitation sit veniam eiusmod sint eiusmod veniam cillum consectetur.
+				Dolore excepteur in et ut excepteur ex. Nulla reprehenderit aute velit amet irure irure dolore sint
+				anim esse quis tempor do incididunt. Deserunt sint reprehenderit anim ullamco deserunt mollit. Lorem
+				commodo laborum quis aute. Ad sit quis labore nisi sint dolore id. Anim proident cillum amet laboris
+				consequat nisi exercitation ipsum labore in ea consequat duis ullamco. Occaecat ex exercitation
+				pariatur fugiat ex veniam nulla Lorem laboris nostrud sunt reprehenderit incididunt. Aliquip
+				incididunt officia veniam nisi eiusmod sint duis ea id aute do elit aliquip officia. Duis voluptate
+				veniam fugiat incididunt est. Laborum magna quis nostrud sint aliquip enim incididunt velit occaecat
+				minim. Cillum cillum ipsum tempor ea. Tempor deserunt nulla eiusmod magna culpa do laboris. Incididunt
+				esse cillum non deserunt officia incididunt minim. Ex laborum in fugiat sint quis tempor mollit
+				ullamco irure deserunt. Irure aliqua id esse nisi eu incididunt pariatur ut aliqua voluptate est
+				magna.
+			</Link>
+		</div>
+	);
+}
