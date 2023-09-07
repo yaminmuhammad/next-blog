fdec0d9eaff4ea0323edd301d9a3bfc97141c9eb
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-09-07 21:04:40 +0700

    add humberger btn 5216

--- src/app/components/authLinks/AuthLinks.jsx
+++ src/app/components/authLinks/AuthLinks.jsx
@@ -1,18 +1,46 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./authLinks.module.css";
+import { useState } from "react";
 
 const AuthLinks = () => {
+	const [open, setOpen] = useState(false);
 	// temporary
-	const status = "authenticated";
+	const status = "not authenticated";
 	return (
 		<>
 			{status === "not authenticated" ? (
-				<Link href="/login">Login</Link>
+				<Link href="/login" className={styles.link}>
+					Login
+				</Link>
 			) : (
 				<>
-					<Link href="/write">Write</Link>
+					<Link href="/write" className={styles.link}>
+						Write
+					</Link>
 					<span className={styles.link}>Logout</span>{" "}
 				</>
+			)}
+			<div className={styles.burger} onClick={() => setOpen(!open)}>
+				<div className={styles.line}></div>
+				<div className={styles.line}></div>
+				<div className={styles.line}></div>
+			</div>
+			{open && (
+				<div className={styles.responsiveMenu}>
+					<Link href="/">Homepage</Link>
+					<Link href="/">About</Link>
+					<Link href="/">Contact</Link>
+					{status === "not authenticated" ? (
+						<Link href="/login">Login</Link>
+					) : (
+						<>
+							<Link href="/write">Write</Link>
+							<span className={styles.link}>Logout</span>{" "}
+						</>
+					)}
+				</div>
 			)}
 		</>
 	);
