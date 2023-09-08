85f8fe5e5abc632e86e465d220bb2ac0f66b9117
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-09-08 11:03:09 +0700

    add category component

--- src/app/components/categoryList/CategoryList.jsx
+++ src/app/components/categoryList/CategoryList.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import styles from "./categoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const CategoryList = () => {
-	return <div className={styles.container}>CategoryList</div>;
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.title}>Popular Categories</h1>
+			<div className={styles.categories}>
+				<Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+					<Image src="/style.png" width={32} height={32} className={styles.image} />
+					style
+				</Link>
+				<Link href="/blog?cat=style" className={`${styles.category} ${styles.fashion}`}>
+					<Image src="/style.png" width={32} height={32} className={styles.image} />
+					fasion
+				</Link>
+				<Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+					<Image src="/style.png" width={32} height={32} className={styles.image} />
+					style
+				</Link>
+				<Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+					<Image src="/style.png" width={32} height={32} className={styles.image} />
+					style
+				</Link>
+				<Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+					<Image src="/style.png" width={32} height={32} className={styles.image} />
+					style
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default CategoryList;
--- src/app/components/featured/Featured.jsx
+++ src/app/components/featured/Featured.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import styles from "./featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
-	return <div className={styles.container}>Featured</div>;
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.title}>
+				<b>Hey, yamin dev here!</b> Discover my stories and creativity ideas.
+			</h1>
+			<div className={styles.post}>
+				<div className={styles.imgContainer}>
+					<Image src="/p1.jpeg" alt="" fill />
+				</div>
+				<div className={styles.textContainer}>
+					<h1 className={styles.postTitle}>Lorem Ipsum dolor sit amet alim consectetur adipiscing elit.</h1>
+					<p className={styles.postDescription}>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quidem, voluptatum, quod, quas
+						voluptatem quia quibusdam aspernatur, voluptate voluptates quos doloribus. Quisquam quidem,
+						voluptatum, quod, quas voluptatem quia quibusdam aspernatur,
+					</p>
+					<button className={styles.button}>Read More</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Featured;
