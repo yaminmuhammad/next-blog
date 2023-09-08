34c62b51b4dc254a76122e882ae41149eefb9281
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-09-08 15:41:50 +0700

    fix responsive 10609

--- src/app/components/categoryList/CategoryList.jsx
+++ src/app/components/categoryList/CategoryList.jsx
@@ -9,25 +9,28 @@ const CategoryList = () => {
 			<h1 className={styles.title}>Popular Categories</h1>
 			<div className={styles.categories}>
 				<Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
-					<Image src="/style.png" width={32} height={32} className={styles.image} />
+					<Image src="/style.png" alt="" width={32} height={32} className={styles.image} />
 					style
 				</Link>
-				<Link href="/blog?cat=style" className={`${styles.category} ${styles.fashion}`}>
-					<Image src="/style.png" width={32} height={32} className={styles.image} />
+				<Link href="/blog" className={`${styles.category} ${styles.fashion}`}>
+					<Image src="/fashion.png" alt="" width={32} height={32} className={styles.image} />
 					fasion
 				</Link>
-				<Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
-					<Image src="/style.png" width={32} height={32} className={styles.image} />
-					style
+				<Link href="/blog" className={`${styles.category} ${styles.food}`}>
+					<Image src="/food.png" alt="" width={32} height={32} className={styles.image} />
+					food
 				</Link>
-				{/*  */}
-				<Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
-					<Image src="/style.png" width={32} height={32} className={styles.image} />
-					style
+				<Link href="/blog" className={`${styles.category} ${styles.travel}`}>
+					<Image src="/travel.png" alt="" width={32} height={32} className={styles.image} />
+					travel
 				</Link>
-				<Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
-					<Image src="/style.png" width={32} height={32} className={styles.image} />
-					style
+				<Link href="/blog" className={`${styles.category} ${styles.culture}`}>
+					<Image src="/culture.png" alt="" width={32} height={32} className={styles.image} />
+					culture
+				</Link>
+				<Link href="/blog" className={`${styles.category} ${styles.coding}`}>
+					<Image src="/coding.png" alt="" width={32} height={32} className={styles.image} />
+					coding
 				</Link>
 			</div>
 		</div>
