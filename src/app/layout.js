faec8d16773c5eee5ef03e0c448e5fb07b64c14e
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-09-13 05:06:22 +0700

    feat signin google oauth 23330

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import ThemeProvider from "@/provider/ThemeProvider";
+import AuthProvider from "@/provider/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,17 +17,19 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				<ThemeContextProvider>
-					<ThemeProvider>
-						<div className="container">
-							<div className="wrapper">
-								<Navbar />
-								{children}
-								<Footer />
+				<AuthProvider>
+					<ThemeContextProvider>
+						<ThemeProvider>
+							<div className="container">
+								<div className="wrapper">
+									<Navbar />
+									{children}
+									<Footer />
+								</div>
 							</div>
-						</div>
-					</ThemeProvider>
-				</ThemeContextProvider>
+						</ThemeProvider>
+					</ThemeContextProvider>
+				</AuthProvider>
 			</body>
 		</html>
 	);
--- src/provider/ThemeProvider.jsx
+++ src/provider/ThemeProvider.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ThemeContext } from "@/context/ThemeContext";
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 const ThemeProvider = ({ children }) => {
 	const { theme } = useContext(ThemeContext);
